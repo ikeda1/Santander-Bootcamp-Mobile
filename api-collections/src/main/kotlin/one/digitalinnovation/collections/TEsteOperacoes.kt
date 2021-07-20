12153577c1109d43afc62ceece5ca24416a3b0a7
ikeda1 <pedroikeda1@gmail.com>
2021-07-20 16:28:49 -0300

    Adicionado novas operacoes no TesteOperacoes test class

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TEsteOperacoes.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TEsteOperacoes.kt
@@ -15,4 +15,18 @@ fun main() {
     val salariosMaiorQue2500 = salarios.filter {it > 2500.0}
     println("----------------------------------------")
     salariosMaiorQue2500.forEach {println(it)}
+
+    println("----------------------------------------")
+    // Imprime quantidade de valores no intervalo de 2000 a 5000
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("----------------------------------------")
+    // Imprime se encontrar o valor exato passado no find
+    println(salarios.find {it == 2250.0})
+    println(salarios.find {it == 500.0})
+
+    println("----------------------------------------")
+    // Imprime valor booleano true ou false conforme a expressao
+    println(salarios.any {it == 1000.0})
+    println(salarios.any {it == 50.0})
 }
