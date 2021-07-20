4fc35ed656370f91f621d72a3ed80b9f7f9adcc6
ikeda1 <pedroikeda1@gmail.com>
2021-07-20 15:42:40 -0300

    Criado int array test class

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
@@ -1,5 +1,34 @@
 package one.digitalinnovation.collections
 
 fun main() {
+    val values = IntArray(5)
 
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for (valor in values) {
+        println(valor)
+    }
+
+    // Outra forma utilizando expressão lambda "forEach"
+    println("-----------------------")
+    values.forEach { valor ->
+        println(valor)
+    }
+
+    // Outra forma utilizando os indices
+    println("-----------------------")
+    for (index in values.indices) {
+        println(values[index])
+    }
+
+    // Outra forma utilizando a ordenação do .sort()
+    println("-----------------------")
+    values.sort()
+    for (valor in values) {
+        println(valor)
+    }
 }
