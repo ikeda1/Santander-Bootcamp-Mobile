38584d4f9a01ce3a72bcb40295df26d3561bf936
ikeda1 <pedroikeda1@gmail.com>
2021-08-11 12:58:31 -0300

    criado app listaDeContatos

---
+++ listaContatos/app/src/main/java/com/example/listacontatos/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.listacontatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+    private val adapter = ContactAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindView()
+        updateList()
+    }
+
+    private fun bindView() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Pedro Ikeda",
+                    "(00) 0000-0000",
+                    "img.png"
+                ),
+                Contact(
+                    "Pedro Ikeda",
+                    "(00) 0000-0000",
+                    "img.png"
+                )
+            )
+        )
+    }
+}
